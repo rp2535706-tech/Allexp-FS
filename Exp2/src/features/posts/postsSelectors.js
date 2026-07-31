de8a65a3e6afffe8c5b87e8738c2047b348d5af6
rp2535706-tech <rp2535706@gmail.com>
2026-07-31 22:49:41 +0530

    Update postsSelectors.js

--- Exp2/src/features/posts/postsSelectors.js
+++ Exp2/src/features/posts/postsSelectors.js
@@ -1,11 +1,25 @@
-import { createSelector } from "reselect";
+export const selectPosts = (state) => state.posts.posts;
 
-const selectPosts = (state) => state.posts.entities;
+export const selectPlatform = (state) =>
+  state.posts.selectedPlatform;
 
-export const selectShortPosts = createSelector(
-  [selectPosts],
-  (posts) =>
-    Object.values(posts).filter(
-      (post) => post && post.body.length < 100
-    )
-);
+export const selectSearch = (state) =>
+  state.posts.search;
+
+export const selectFilteredPosts = (state) => {
+  const posts = state.posts.posts;
+  const platform = state.posts.selectedPlatform;
+  const search = state.posts.search.toLowerCase();
+
+  return posts.filter((post) => {
+    const platformMatch =
+      platform === "All" || post.platform === platform;
+
+    const searchMatch =
+      post.title.toLowerCase().includes(search) ||
+      post.author.toLowerCase().includes(search) ||
+      post.description.toLowerCase().includes(search);
+
+    return platformMatch && searchMatch;
+  });
+};
