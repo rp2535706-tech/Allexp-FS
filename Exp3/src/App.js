466dc43d94b9af2da1d13822734294bb63ad8bb6
rp2535706-tech <rp2535706@gmail.com>
2026-08-12 14:57:54 +0530

    Update App.js

--- Exp3/src/App.js
+++ Exp3/src/App.js
@@ -1,1 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
