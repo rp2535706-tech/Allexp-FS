7f5ec5e82f3448bca76b32ecda4d5fbc51ca250a
rp2535706-tech <rp2535706@gmail.com>
2026-08-12 15:02:41 +0530

    Update users.js

--- Exp3/src/users.js
+++ Exp3/src/users.js
@@ -1,1 +1,17 @@
-
+export const users = [
+  {
+    id: "admin",
+    password: "admin123",
+    role: "Admin",
+  },
+  {
+    id: "editor",
+    password: "editor123",
+    role: "Editor",
+  },
+  {
+    id: "viewer",
+    password: "viewer123",
+    role: "Viewer",
+  },
+];
