11c29ecf1adca77536ecb5406bb3c46a680d333f
rp2535706-tech <rp2535706@gmail.com>
2026-09-03 09:41:45 +0530

    Update handlers.js

--- Exp4/src/mocks/handlers.js
+++ Exp4/src/mocks/handlers.js
@@ -1,1 +1,23 @@
+import { http, HttpResponse } from "msw";
 
+export const handlers = [
+  http.get("/api/events", () => {
+    return HttpResponse.json([
+      {
+        id: 1,
+        title: "Team Meeting",
+        date: "2026-08-30",
+      },
+      {
+        id: 2,
+        title: "Project Review",
+        date: "2026-08-31",
+      },
+      {
+        id: 3,
+        title: "Presentation",
+        date: "2026-08-30",
+      },
+    ]);
+  }),
+];
